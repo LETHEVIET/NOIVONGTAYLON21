e3491ed734e1b815c702bfb7a698553d7449a9a2
LE THE VIET <winviet10@gmail.com>
2021-07-23 17:04:36 +0700

    ver 2.0 now can share to facebook

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
-const path = __dirname + '/app/views';
+const fs = require("fs");  
+const path = __dirname + '/app/views/';
 
 const app = express();
 
@@ -15,15 +15,10 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-/*
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
-*/
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -31,6 +26,7 @@ app.use(function(req, res, next) {
 });
 
 const dp = require("./app/models");
+const { person } = require("./app/models");
 
 dp.mongoose
     .connect(dp.url, {
@@ -45,22 +41,67 @@ dp.mongoose
         process.exit();
     });
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Vietle application." });
-});
 
+const pathToIndex = __dirname + '/app/views/' + "index.html";
+app.get("/", (req, res) => {
+  const raw = fs.readFileSync(pathToIndex)
+  const pageTitle = "Homepage - Welcome to my page"
+  const updated = raw.replace("__PAGE_META__", `<title>${pageTitle}</title>`)
+  res.send(updated)
+})
 
 //db.sequelize.sync();
 
-app.get('/', function (req, res){
-  res.sendFile(path + "index.html")
+app.get('/*', function (req, res){
+  //res.sendFile(path + "index.html")
+  const pageTitle = "Homepage - Welcome to my page"
+  console.log(req.query);
+
+  if (req.query.id == null){
+    return res.sendFile(path + "index.html")
+  }
+
+  if (dp.mongoose.connection.readyState == 1){
+    
+    const Person = dp.person;
+    Person.findById(req.query.id)
+        .then(data => {
+            if (!data)
+              res.sendFile(path + "index.html")
+            else  {
+              fs.readFile(pathToIndex, 'utf8', (err, htmlData) => {
+                if (err) {
+                    console.error('Error during file reading', err);
+                    return res.status(404).end()
+                }
+
+                htmlData = htmlData
+                    .replace("__META_OG_TITLE__", data.name)
+                    .replace("__META_DESCRIPTION__", data.description)
+                    .replace("__META_OG_IMAGE__", data.card)
+                return res.send(htmlData);
+              });
+            }
+        })
+        .catch(err => {
+        });
+  }
+
+  fs.readFile(pathToIndex, 'utf8', (err, htmlData) => {
+    if (err) {
+        console.error('Error during file reading', err);
+        return res.status(404).end()
+    }
+    // TODO get person info
+
+    // TODO inject meta tags
+  });
+  
 })
 
 
 require("./app/routes/person.routes")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
